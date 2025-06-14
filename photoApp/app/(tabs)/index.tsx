fdd3511d25db392a23cf63bd74164ca30842ceb7
Dinesh <dineshsrinivasanr77529@gmail.com>
2025-06-15 00:28:00 +0530

    Pagination / infinited scrolling in app

--- photoApp/app/(tabs)/index.tsx
+++ photoApp/app/(tabs)/index.tsx
@@ -7,6 +7,10 @@ import { useEffect, useState } from "react";
 export default function HomeScreen() {
   const [permissionResponse, requestPermission] = MediaLibrary.usePermissions();
 
+  const [hasnextPage, sethasnextPage] = useState(true);
+  const[endCursor, setendCursor] = useState<string>();
+  const[loading, setLoading] = useState(false);
+
   const[localAssets, setlocalAssets] = useState<MediaLibrary.Asset[]>([]);
   useEffect(() => {
     if (permissionResponse?.status !== 'granted') {
@@ -21,10 +25,18 @@ export default function HomeScreen() {
   }, [permissionResponse]);
 
   const loadAsset = async () => {
-    const assetPage = await MediaLibrary.getAssetsAsync(); // FIXED here
+    if (loading || !hasnextPage){
+      return;
+    }
+
+    setLoading(true);
+    const assetPage = await MediaLibrary.getAssetsAsync({after: endCursor}); // FIXED here
     console.log(JSON.stringify(assetPage, null, 2));
 
-    setlocalAssets(assetPage.assets);
+    setlocalAssets((existingitem) => [...existingitem, ...assetPage.assets]);
+    sethasnextPage(assetPage.hasNextPage);
+    setendCursor(assetPage.endCursor);
+    setLoading(false);
   };
 
   console.log(permissionResponse);
@@ -36,10 +48,15 @@ export default function HomeScreen() {
       numColumns={4}
       columnWrapperStyle={{ gap: 1, justifyContent: 'space-between', marginBottom: 1 }} // horizontal + vertical gap
       contentContainerStyle={{ padding: 1 }} 
+      onEndReached={loadAsset}
+      onEndReachedThreshold={2}
+      refreshing={loading}
       renderItem={({item}) => 
                     // <Text>{item.uri}</Text>
                     <Image source={{uri: item.uri}} style ={{width: '25%', aspectRatio: 1}}/>
-                  } />
+                  }
+     />
+     {/* {hasnextPage && <Text onPress={loadAsset} style ={{color: 'white', textAlign: 'center', padding: 10}}>Load next</Text>} */}
     </>
   );
 }
